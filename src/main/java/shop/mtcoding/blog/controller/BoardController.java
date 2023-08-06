603f9e52a29ebb5adb066ef02e3530b02048d96c
bbarkse93 <bbarkse93@gmail.com>
2023-08-06 15:47:49 +0900

    화면구현 완료

--- src/main/java/shop/mtcoding/blog/controller/BoardController.java
+++ src/main/java/shop/mtcoding/blog/controller/BoardController.java
@@ -4,10 +4,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class TestController {
+public class BoardController {
 
-    @GetMapping("/")
-    public String Test() {
-        return "index";
+    @GetMapping("/board/writeForm")
+    public String writeForm(){
+        return "/board/writeForm";
     }
 }
---
+++ src/main/java/shop/mtcoding/blog/controller/UserController.java
@@ -0,0 +1,32 @@
+package shop.mtcoding.blog.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class UserController {
+
+    @GetMapping("/")
+    public String home(){
+        return "index";
+    }
+
+    @GetMapping("/loginForm")
+    public String loginForm(){
+        return "user/loginForm";
+    }
+
+    @GetMapping("/joinForm")
+    public String joinForm(){
+        return "user/joinForm";
+    }
+    @GetMapping("/user/updateForm")
+    public String updateForm(){
+        return "user/updateForm";
+    }
+
+    @GetMapping("/logout")
+    public String logout(){
+        return "redirect:/";
+    }
+}
